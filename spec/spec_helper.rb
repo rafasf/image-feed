07b059bba4739da047612ad2f60a4221ff501a98
Rafael Ferreira <rafaelsf@gmail.com>
2013-10-22 16:06:14 -0200

    Check how files are loaded in snap

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-Dir.glob(File.expand_path(File.dirname(__FILE__) +  '/../src/*.rb')).each { |file| require file }
+Dir.glob(File.expand_path(File.dirname(__FILE__) +  '/../src/*.rb')).each { |file| require file; p file }
