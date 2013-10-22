e06116eaef54d54ee6bcd96c70247641e7f7783a
Rafael Ferreira <rafaelsf@gmail.com>
2013-10-22 16:30:36 -0200

    Revert "Check how files are loaded in snap"
    This reverts commit 07b059bba4739da047612ad2f60a4221ff501a98.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-Dir.glob(File.expand_path(File.dirname(__FILE__) +  '/../src/*.rb')).each { |file| require file; p file }
+Dir.glob(File.expand_path(File.dirname(__FILE__) +  '/../src/*.rb')).each { |file| require file }
